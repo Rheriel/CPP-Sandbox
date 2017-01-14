a478d1679d2ba18fb18f5b65929fc3919541c05e
Jorge Abraham Pérez Gómez <rheriel@hotmail.co.jp>
2017-01-13 18:14:20 -0600

    Some training on the pointers concept.

--- Training/src/Training.cpp
+++ Training/src/Training.cpp
@@ -9,7 +9,28 @@
 #include <iostream>
 using namespace std;
 
+void passByValue(int val);
+void passByReference(int *val);
+
 int main() {
 	cout << "Hello, World!" << endl; // prints Hello, World!
+
+	int value = 10;
+	int reference = 10;
+
+	passByValue(value);
+	passByReference(&reference);
+
+	cout << "Value after function: "<< value << endl;
+	cout << "Reference after function: "<< reference << endl;
+
 	return 0;
+}
+
+void passByValue(int val){
+	val = 20;
+}
+
+void passByReference(int *val){
+	*val = 20;
 }
