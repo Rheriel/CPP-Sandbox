d86eb12b2734f1c2a14525fe7b39efc1fb54fa3b
Jorge Abraham Pérez Gómez <rheriel@hotmail.co.jp>
2017-01-14 22:51:43 -0600

    Attempt to fix QuickSort using pointers.

--- QuickSort/src/QuickSort.cpp
+++ QuickSort/src/QuickSort.cpp
@@ -12,63 +12,11 @@ using namespace std;
 
 const int NUMBERS_TO_SORT = 10;
 
-void randomlyPopulateArray(int a*){
-	int aLength = sizeof(a)/sizeof(a[0]);
-
-	// Initialize randomize
-	srand(time(NULL));
-
-	for (int i = 0; i < aLength; i++){
-		a[i] = rand() % 10 + 1;
-	}
-}
-
-void printArray(int a*){
-	int aLength = sizeof(a)/sizeof(a[0]);
-
-	for (int i = 0; i < aLength - 1; i++){
-		cout << a[i] << ", ";
-	}
-
-	cout << a[aLength - 1] << endl;
-}
-
-void swap(int i*, int j*){
-	int temp* = i;
-	i = j;
-	j = temp;
-}
-
-int* partition(int *left, int *right, int *pivot){
-
-	while(&left <= &right){
-		while (left < pivot){
-			left++;
-		}
-		while (right > pivot){
-			right--;
-		}
-
-		if (left <= right){
-			swap(left, right);
-			left++;
-			right--;
-		}
-	}
-
-	return left;
-}
-
-void quickSort(int *left, int *right){
-	if (left >= right){
-		return;
-	}
-
-	int pivot = (left + right) / 2;
-	int *index = partition(left, right, pivot);
-	quickSort(left, index - 1);
-	quickSort(index, right);
-}
+void randomlyPopulateArray(int *a);
+void printArray(int *a);
+void quickSort(int *arr, int left, int right);
+void swap(int *arr, int i, int j);
+int partition(int *arr, int left, int right, int pivot);
 
 int main(){
 	int a[NUMBERS_TO_SORT];
@@ -81,7 +29,7 @@ int main(){
 	// cout << "Size of the array: " << sizeof(a)/sizeof(a[0]);
 
 	// Move on.
-	randomlyPopulateArray(&a);
+	randomlyPopulateArray(a);
 	cout << "Array before QuickSort: ";
 	printArray(&a);
 
@@ -91,6 +39,64 @@ int main(){
 	printArray(&a);
 
 	return 0;
+}
+
+void randomlyPopulateArray(int *a){
+	int aLength = sizeof(a)/sizeof(a[0]);
+
+	// Initialize randomize
+	srand(time(NULL));
+
+	for (int i = 0; i < aLength; i++){
+		a[i] = rand() % 10 + 1;
+	}
+}
+
+void printArray(int *a){
+	int aLength = sizeof(a)/sizeof(a[0]);
+
+	for (int i = 0; i < aLength - 1; i++){
+		cout << a[i] << ", ";
+	}
+
+	cout << a[aLength - 1] << endl;
+}
+
+void swap(int *arr, int i, int j){
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+int partition(int *arr, int left, int right, int pivot){
+
+	while(left <= right){
+		while (arr[left] < pivot){
+			left++;
+		}
+		while (arr[right] > pivot){
+			right--;
+		}
+
+		if (left <= right){
+			swap(arr, left, right);
+			left++;
+			right--;
+		}
+	}
+
+	return left;
+}
+
+void quickSort(int *arr, int left, int right){
+	if (left >= right){
+		return;
+	}
+
+	int pivot = (left + right) / 2;
+	int index = partition(arr, left, right, pivot);
+	quickSort(arr, left, index - 1);
+	quickSort(arr, index, right);
 }
 
 
