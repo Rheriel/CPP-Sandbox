9c4885abf80c6a009577a6f473048ba1e1a2f19e
Jorge Abraham Pérez Gómez <rheriel@hotmail.co.jp>
2017-01-15 21:19:10 -0600

    Changes based on some research on good practices.

--- QuickSort/src/QuickSort.cpp
+++ QuickSort/src/QuickSort.cpp
@@ -8,15 +8,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-using namespace std;
+// Avoid Namespace pollution: https://gcc.gnu.org/ml/libstdc++/2000-09/msg00067.html
+// using namespace std;
 
 const int NUMBERS_TO_SORT = 10;
 
-void randomlyPopulateArray(int *a);
-void printArray(int *a);
+void randomlyPopulateArray(int *a, int size);
+void printArray(int *a, int size);
 void quickSort(int *arr, int left, int right);
-void swap(int *arr, int i, int j);
-int partition(int *arr, int left, int right, int pivot);
+int partition(int *arr, const int left, const int right, const int pivot);
 
 int main(){
 	int a[NUMBERS_TO_SORT];
@@ -29,57 +29,52 @@ int main(){
 	// cout << "Size of the array: " << sizeof(a)/sizeof(a[0]);
 
 	// Move on.
-	randomlyPopulateArray(a);
-	cout << "Array before QuickSort: ";
-	printArray(&a);
+	randomlyPopulateArray(a, NUMBERS_TO_SORT);
+	std::cout << "Array before QuickSort: ";
+	printArray(a, NUMBERS_TO_SORT);
 
-	quickSort(&a, low, high);
+	quickSort(a, low, high);
 
-	cout << "Array after QuickSort: ";
-	printArray(&a);
+	std::cout << "Array after QuickSort: ";
+	printArray(a, NUMBERS_TO_SORT);
 
 	return 0;
 }
 
-void randomlyPopulateArray(int *a){
-	int aLength = sizeof(a)/sizeof(a[0]);
+void randomlyPopulateArray(int *a, int size){
 
 	// Initialize randomize
 	srand(time(NULL));
 
-	for (int i = 0; i < aLength; i++){
+	int i = 0;
+	while (i < size) {
 		a[i] = rand() % 10 + 1;
+		i++;
 	}
 }
 
-void printArray(int *a){
-	int aLength = sizeof(a)/sizeof(a[0]);
-
-	for (int i = 0; i < aLength - 1; i++){
-		cout << a[i] << ", ";
+void printArray(int *a, int size){
+	int i = 0;
+	while (i < size - 1) {
+		std::cout << a[i] << ", ";
+		i++;
 	}
 
-	cout << a[aLength - 1] << endl;
-}
-
-void swap(int *arr, int i, int j){
-	int temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
+	std::cout << a[size - 1] << std::endl;
 }
 
 int partition(int *arr, int left, int right, int pivot){
 
 	while(left <= right){
-		while (arr[left] < pivot){
+		while (left <= right && arr[left] <= pivot){
 			left++;
 		}
-		while (arr[right] > pivot){
+		while (left <= right && arr[right] > pivot){
 			right--;
 		}
 
-		if (left <= right){
-			swap(arr, left, right);
+		if (left < right){
+			std::swap(arr[left], arr[right]);
 			left++;
 			right--;
 		}
@@ -88,12 +83,18 @@ int partition(int *arr, int left, int right, int pivot){
 	return left;
 }
 
-void quickSort(int *arr, int left, int right){
+void quickSort(int *arr, const int left, const int right){
 	if (left >= right){
 		return;
 	}
 
-	int pivot = (left + right) / 2;
+	// This is a bug:
+	// https://research.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+	// int pivot = (left + right) / 2;
+
+    // Use this instead:
+	int pivot = left + ((right - left) / 2);
+	std::cout << "Pivot: " << pivot << std::endl;
 	int index = partition(arr, left, right, pivot);
 	quickSort(arr, left, index - 1);
 	quickSort(arr, index, right);
