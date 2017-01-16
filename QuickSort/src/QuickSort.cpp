876c41d7dd7be890de8e4d57621913154767f48b
Jorge Abraham Pérez Gómez <rheriel@hotmail.co.jp>
2017-01-15 21:36:31 -0600

    Completed the QuickSort algorithm.

--- QuickSort/src/QuickSort.cpp
+++ QuickSort/src/QuickSort.cpp
@@ -13,10 +13,10 @@
 
 const int NUMBERS_TO_SORT = 10;
 
-void randomlyPopulateArray(int *a, int size);
-void printArray(int *a, int size);
-void quickSort(int *arr, int left, int right);
-int partition(int *arr, const int left, const int right, const int pivot);
+void randomlyPopulateArray(int *a, const int size);
+void printArray(int *a, const int size);
+void quickSort(int *arr, const int left, const int right);
+int partition(int *arr, const int left, const int right);
 
 int main(){
 	int a[NUMBERS_TO_SORT];
@@ -41,7 +41,7 @@ int main(){
 	return 0;
 }
 
-void randomlyPopulateArray(int *a, int size){
+void randomlyPopulateArray(int *a, const int size){
 
 	// Initialize randomize
 	srand(time(NULL));
@@ -53,7 +53,7 @@ void randomlyPopulateArray(int *a, int size){
 	}
 }
 
-void printArray(int *a, int size){
+void printArray(int *a, const int size){
 	int i = 0;
 	while (i < size - 1) {
 		std::cout << a[i] << ", ";
@@ -63,24 +63,41 @@ void printArray(int *a, int size){
 	std::cout << a[size - 1] << std::endl;
 }
 
-int partition(int *arr, int left, int right, int pivot){
+int partition(int *arr, const int left, const int right){
 
-	while(left <= right){
-		while (left <= right && arr[left] <= pivot){
-			left++;
+	// This is a bug:
+	// https://research.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+	// const int pivot = (left + right) / 2;
+
+    // Use this instead:
+    const int mid = left + ((right - left) / 2);
+    const int pivot = arr[mid];
+
+    std::cout << "Pivot: " << pivot << std::endl;
+
+    // We move the pivot to the front.
+    std::swap(arr[mid], arr[left]);
+
+	int i = left + 1; // The pivot is now the first element.
+	int j = right;
+
+	while(i <= j){
+		while (i <= j && arr[i] <= pivot){
+			i++;
 		}
-		while (left <= right && arr[right] > pivot){
-			right--;
+		while (i <= j && arr[j] > pivot){
+			j--;
 		}
 
-		if (left < right){
-			std::swap(arr[left], arr[right]);
-			left++;
-			right--;
+		if (i < j){
+			std::swap(arr[i], arr[j]);
 		}
 	}
 
-	return left;
+	// We move the pivot to its final position.
+	std::swap(arr[i - 1], arr[left]);
+
+	return i - 1;
 }
 
 void quickSort(int *arr, const int left, const int right){
@@ -88,16 +105,9 @@ void quickSort(int *arr, const int left, const int right){
 		return;
 	}
 
-	// This is a bug:
-	// https://research.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
-	// int pivot = (left + right) / 2;
-
-    // Use this instead:
-	int pivot = left + ((right - left) / 2);
-	std::cout << "Pivot: " << pivot << std::endl;
-	int index = partition(arr, left, right, pivot);
+	int index = partition(arr, left, right);
 	quickSort(arr, left, index - 1);
-	quickSort(arr, index, right);
+	quickSort(arr, index + 1, right);
 }
 
 
