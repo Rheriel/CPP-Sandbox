136b766fa1648af94a1a751f812f097e9acee35f
Jorge Abraham Pérez Gómez <rheriel@hotmail.co.jp>
2017-01-20 21:26:31 -0600

    Fixed MergeSort logic on copying temp and using Pointers.

--- MergeSort/src/MergeSort.cpp
+++ MergeSort/src/MergeSort.cpp
@@ -7,17 +7,19 @@
 
 #include <iostream>
 
-void mergeSort(int arr[]);
-void mergeSort(int arr[], int temp[], int low, int high);
-void mergeHalves(int arr[], int temp[], int leftStart, int rightEnd);
+void mergeSort(int *arr);
+void mergeSort(int *arr, int *temp, int low, int high);
+void mergeHalves(int *arr, int *temp, int leftStart, int rightEnd);
 
-void mergeSort(int arr[]){
-	int aLength = sizeof(arr) / sizeof(arr[0]);
-	mergeSort(arr, new int[aLength], 0, aLength - 1);
+int ARRAY_SIZE = 10;
+
+void mergeSort(int *arr){
+	mergeSort(arr, new int[ARRAY_SIZE], 0, ARRAY_SIZE - 1);
 }
 
-void mergeSort(int arr[], int temp[], int low, int high){
+void mergeSort(int *arr, int *temp, int low, int high){
 	if (low >= high) return;
+
 	int mid = low + ((high - low) / 2);
 
 	mergeSort(arr, temp, low, mid);
@@ -25,7 +27,7 @@ void mergeSort(int arr[], int temp[], int low, int high){
 	mergeHalves(arr, temp, low, high);
 }
 
-void mergeHalves(int arr[], int temp[], int leftStart, int rightEnd){
+void mergeHalves(int *arr, int *temp, int leftStart, int rightEnd){
 	int left = leftStart;
 	int leftEnd = leftStart + ((rightEnd - leftStart) / 2);
 	int rightStart = leftEnd + 1;
@@ -33,42 +35,41 @@ void mergeHalves(int arr[], int temp[], int leftStart, int rightEnd){
 	int index = leftStart;
 
 	while (left <= leftEnd && right <= rightEnd){
-		if (arr[left] >= arr[right]){
+		if (arr[left] > arr[right]){
 			temp[index++] = arr[right++];
 		} else {
 			temp[index++] = arr[left++];
 		}
 	}
 
-	while (left < leftEnd){
+	while (left <= leftEnd){
 		temp[index++] = arr[left++];
 	}
-	while (right < rightEnd){
+	while (right <= rightEnd){
 		temp[index++] = arr[right++];
 	}
 
-	for (int i = left; i < right; i++){
+	for (int i = leftStart; i <= rightEnd; i++){
 		arr[i] = temp[i];
 	}
 }
 
 int main(){
-	int arr[] = { 5, 1, 2, 4, 6, 9 };
-	int aLength = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = { 5, 1, 2, 4, 6, 9, 3, 8, 7, 0 };
 
-	for(int i = 0; i < aLength - 1; i++){
+	for(int i = 0; i < ARRAY_SIZE - 1; i++){
 		std::cout << arr[i] << ", ";
 	}
-	std::cout << arr[aLength - 1] << std::endl;
+	std::cout << arr[ARRAY_SIZE - 1] << std::endl;
 
 	std::cout << "Calling MergeSort." << std::endl;
 
 	mergeSort(arr);
 
-	for(int i = 0; i < aLength - 1; i++){
+	for(int i = 0; i < ARRAY_SIZE - 1; i++){
 		std::cout << arr[i] << ", ";
 	}
-	std::cout << arr[aLength - 1] << std::endl;
+	std::cout << arr[ARRAY_SIZE - 1] << std::endl;
 
 }
 
