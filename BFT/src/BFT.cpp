f5e6807e4c7bea2ad0c55bde4b433397cd22a935
Jorge Abraham Pérez Gómez <rheriel@hotmail.co.jp>
2017-01-21 23:57:49 -0600

    Creating a BFT example to learn about classes in C++.

---
+++ BFT/src/BFT.cpp
@@ -0,0 +1,20 @@
+/*
+ * BFT.cpp
+ *
+ *  Created on: 21/01/2017
+ *      Author: Ban
+ */
+
+#include <iostream>
+
+/**
+ * This will be a Breadth-First Traversal example, I'll have to learn to create classes
+ * for this exercise.
+ *
+ * Stay put...
+ */
+int main(){
+
+}
+
+
